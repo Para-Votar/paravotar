6739aacd525d6cdf14947e6a4d39a7164c31851d
Christian Rodriguez <christian@withmaza.com>
2024-09-17 12:33:53 -0400

    add headers

--- src/pages/papeletas.tsx
+++ src/pages/papeletas.tsx
@@ -1,9 +1,11 @@
 import { useLocation, useNavigate } from "react-router-dom"
 import { precintList } from "../packages/practica/constants"
-import { Button, Layout } from "../components"
+import { Button, Container, Layout, Typography } from "../components"
 import Dropdown from "../components/button-dropdown"
 import { useState } from "react"
 import getNormalizedName from "../packages/practica/services/normalize-name"
+import SEO from "../components/seo"
+import { useTranslation } from "react-i18next"
 
 function getBallots() {
   return precintList.reduce<{ [municipality: string]: string[] }>(
@@ -31,38 +33,58 @@ function getBallots() {
 const municipalityBallots = getBallots()
 
 export default function Papeletas() {
-
+  const { t } = useTranslation()
   const location = useLocation()
-  const navigate = useNavigate();
-  
-  const dropdownOptions = [
-    { value: "estatal", name: "Estatal"},
-  ]
-  Object.entries(municipalityBallots).map(
-    ([municipality, precincts]) => {
-      dropdownOptions.push({ value: `municipal/${getNormalizedName(municipality)}`, name: `${municipality} - Municipal` });
-      precincts.map((precinct) => {
-        dropdownOptions.push({ value: `legislativa/${getNormalizedName(precinct)}`, name: `${municipality} - ${precinct} - Legislativa`});
+  const navigate = useNavigate()
+
+  const dropdownOptions = [{ value: "estatal", name: "Estatal" }]
+  Object.entries(municipalityBallots).map(([municipality, precincts]) => {
+    dropdownOptions.push({
+      value: `municipal/${getNormalizedName(municipality)}`,
+      name: `${municipality} - Municipal`,
+    })
+    precincts.map((precinct) => {
+      dropdownOptions.push({
+        value: `legislativa/${getNormalizedName(precinct)}`,
+        name: `${municipality} - ${precinct} - Legislativa`,
       })
-    }
-  )
+    })
+  })
 
-  const [selectedBallot, setSelectedBallot] = useState('estatal');
-
+  const [selectedBallot, setSelectedBallot] = useState("estatal")
 
   return (
     <Layout location={location}>
-    <div className='m-4'>
-      <h1>Escoge una papeleta</h1>
-      <Dropdown
-        options={dropdownOptions}
-        selectedOption={'estatal'}
-        onSelect={(t: string) => {
-          setSelectedBallot(t);
-        }}
-      />
-      <Button className="my-4" onClick={() => navigate(`/papeletas/${selectedBallot}`)}>Navegar a la papeleta</Button>
-      </div>
+      <SEO title="Lista de Papeletas de Puerto Rico (2024)" />
+      <Container className="m-4 text-center" id="practica-tu-voto">
+        <Typography tag="h2" variant="h3" className="uppercase">
+          {t("papeletas.escoge-una-papeleta")}
+        </Typography>
+        <Typography
+          tag="h3"
+          variant="h2"
+          weight="base"
+          className="font-normal mt-4"
+        >
+          {t("papeletas.escoge-y-ve-directo-a-la-papeleta")}
+        </Typography>
+        <Typography tag="p" variant="p" className="my-4">
+          {t("papeletas.papeletas-no-interactivas")}
+        </Typography>
+        <Dropdown
+          options={dropdownOptions}
+          selectedOption={"estatal"}
+          onSelect={(t: string) => {
+            setSelectedBallot(t)
+          }}
+        />
+        <Button
+          className="my-4"
+          onClick={() => navigate(`/papeletas/${selectedBallot}`)}
+        >
+          Navegar a la papeleta
+        </Button>
+      </Container>
     </Layout>
   )
 }
