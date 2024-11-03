aebd893e7830dc84213a83a52fa8c43c3e890079
Emmanuel Luciano <emma.luciano11@gmail.com>
2024-11-03 17:01:22 -0400

    Updates votes section

--- src/packages/practica/components/make-your-vote-count.tsx
+++ src/packages/practica/components/make-your-vote-count.tsx
@@ -1,9 +1,8 @@
 import { Typography } from "../../../components/index"
-import ValidMark from "../../../assets/images/valid-mark.svg?url"
 import { useTranslation } from "react-i18next"
 
 export default function MakeYourVoteCount() {
-  const { t } = useTranslation();
+  const { t } = useTranslation()
 
   return (
     <>
@@ -21,19 +20,10 @@ export default function MakeYourVoteCount() {
       <div className="mt-8">
         <div className="lg:w-10/12 lg:mx-auto">
           <div className="lg:flex">
-            <div className="w-full lg:w-1/4">
-              <img
-                className="w-32 mx-auto flex-shrink-0"
-                src={ValidMark}
-                alt=""
-              />
-            </div>
-            <div className="mt-4 w-full lg:w-3/4 lg:mt-0">
-              <Typography tag="p" variant="p" className="text-left">
-                {t("practice.the-only-valid-sign")}{" "}
-                <b>{t("practice.your-vote-discarded")}</b>
-              </Typography>
-            </div>
+            <Typography tag="p" variant="p" className="text-center">
+              {t("practice.the-only-valid-sign")} <br />
+              <b>{t("practice.your-vote-discarded")}</b>
+            </Typography>
           </div>
         </div>
       </div>
